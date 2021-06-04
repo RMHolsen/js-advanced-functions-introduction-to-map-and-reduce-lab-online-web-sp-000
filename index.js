9b19a7f4ff3a297c5db56554a3e403def8c7b979
rmholsen <jaguar.kitty@gmail.com>
2021-06-04 17:01:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+const mapToNegativize = (array) => {
+    // for block could also be a while block, basically 'run down the length of the array'
+    for (let i = 0; i < array.length; i++) {
+        array[i] * -1;
+    }
+}
+
+const mapToNoChange = (array) => {
+    return array
+}
+
+const mapToDouble = (array) => {
+
+}
+
+const mapToSquare = (array) => {
+
+}
